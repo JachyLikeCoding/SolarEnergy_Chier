62df23c908472abb27953942905bad1afcf25a9e
JachyCoder <38558193+jachylikecoding@users.noreply.github.com>
2019-03-28 21:33:20 +0800

    ps: Just modify author name.

---
+++ Scene/SolarScene.h
@@ -0,0 +1,20 @@
+//
+// Created by feng on 19-3-28.
+//
+
+#ifndef SOLARENERGY_CHIER_SOLARSCENE_H
+#define SOLARENERGY_CHIER_SOLARSCENE_H
+
+
+#include "Heliostat.cuh"
+#include "Grid.cuh"
+#include "Receiver.cuh"
+#include "Sunray.cuh"
+using namespace std;
+
+
+
+
+
+
+#endif //SOLARENERGY_CHIER_SOLARSCENE_H
